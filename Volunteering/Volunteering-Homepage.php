60ec4980a775a986f7b653d51f09573855f96557
Anthony Edward <Anthony_ehab@hotmail.com>
2025-03-17 08:24:14 +0200

    wrote an example of how migration
    gave an example on how migration into php and organization should look like

---
+++ Volunteering/Volunteering-Homepage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="../CSS Sheets/HomePageStyle.css">
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="icon" type="image/x-icon" href="../CSS Sheets/Images/AboutUsImages/favicon.svg">
+        <title>Types Of Volunteering</title>
+        <link rel="stylesheet" href="../CSS Sheets/FormStyle1.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/c19e8a164c.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../CSS Sheets/aboutNavStyle.css">
+        <h1 id="title" style="font-family: Arial; font-size: 50px; text-align: center; color: lightseagreen;">Types Of Volunteering</h1>
+    </head>
+    <body>
+        <?php
+            include_once '../repeated.php';
+            navBar();
+        ?>
+        <h1><b>Description:</b></h1>
+        <p>
+        <br>Volunteering in hospitals is a noble act that can significantly impact the lives of patients and their families.
+            By dedicating their time and energy, volunteers provide invaluable support,
+            alleviating the burdens faced by both patients and healthcare staff.
+            From offering companionship and emotional support to assisting with administrative tasks,
+            volunteers contribute to a more positive and caring hospital environment.
+            Their efforts can uplift patients' spirits, reduce stress, and accelerate the healing process. Ultimately,
+            hospital volunteers play a vital role in enhancing the overall quality of healthcare and creating a more compassionate healthcare system.</p><hr> 
+             <h1>Types Of Volunteering</h1>
+             <a href="volunteeringUserSide/Medical Volunteering.html" target="_blank"></a>
+             <a href="volunteeringUserSide/Medical Volunteering.html" class="button-link">
+             <img src="../CSS Sheets/Images/Volunteering/Medical Volunteer.jpg" width="200" height="200" alt="Icon" class="button-image" align="center"></a>
+             <label for="myButton">Medical Volunteering</label>
+             <a href="volunteeringUserSide/Office Volunteering.html" target="_blank"></a>
+             <a href="volunteeringUserSide/Office Volunteering.html" class="button-link">
+             <img src="../CSS Sheets/Images/Volunteering/Office Volunteer.jpg" width="200" height="200" alt="Icon" class="button-image" align="center"></a>
+             <label for="myButton">Office Volunteering</label>
+             <a href="volunteeringUserSide/Child Life Volunteering.html" target="_blank"></a>
+             <a href="volunteeringUserSide/Child Life Volunteering.html" class="button-link">
+             <img src="../CSS Sheets/Images/Volunteering/Child Life Volunteer.jpg" width="200" height="200" alt="Icon" class="button-image" align="center"></a>
+             <label for="myButton">Child Life Volunteering</label>
+    </body>
+</html>
