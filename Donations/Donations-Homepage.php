659c6b01e26009f6b8f07e20f46b499dfe352e97
Anthony Edward <Anthony_ehab@hotmail.com>
2025-03-18 21:07:51 +0200

    fixed some dirs in donations

--- Donations/Donations-Homepage.php
+++ Donations/Donations-Homepage.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="CSS Sheets/HomePageStyle.css">
+        <link rel="stylesheet" href="../CSS Sheets/HomePageStyle.css">
         <meta charset='utf-8'>
         <meta http-equiv='X-UA-Compatible'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel="icon" type="image/x-icon" href="CSS Sheets/Images/AboutUsImages/favicon.svg">
+        <link rel="icon" type="image/x-icon" href="../CSS Sheets/Images/AboutUsImages/favicon.svg">
         <title>Types Of Donations</title>
-        <link rel="stylesheet" href="FormStyle1.css">
+        <link rel="stylesheet" href="../FormStyle1.css">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/c19e8a164c.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="CSS Sheets/aboutNavStyle.css">
+        <link rel="stylesheet" href="../CSS Sheets/aboutNavStyle.css">
         <div id="title"><h1 id="title" style="font-family: Arial; font-size: 50px; text-align: center; color: lightseagreen;">Donations</h1>
         <title>Donations</title></div>
     </head>
