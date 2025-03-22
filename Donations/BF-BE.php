225e43ea1417f7497dbc739f9a7919c06eb01195
Selimeleraky <Selimeleraky66@gmail.com>
2025-03-22 22:29:30 +0200

    Enhancing Php
    Making data stay implemented in the table after refreshing the page

---
+++ Donations/BF-BE.php
@@ -0,0 +1,20 @@
+<?php
+$DonationID=$_POST["DonationID"];
+$DonatorsName= $_POST["DonatorsName"];
+$BloodType =$_POST["BloodType"];
+$DonationDate = $_POST["DonationDate"];
+$DonationCountry = $_POST["DonationCountry"];
+
+
+
+ $data = "DonationID : $DonationID\n" .
+         "DonatorsName :  $DonatorsName\n" .
+         "BloodType: $BloodType\n" .
+         "DonationDate: $DonationDate\n" .
+         "DonationCountry: $DonationCountry\n";
+
+$file = fopen("BF.txt","a");
+fwrite($file,$data);
+fclose($file);
+echo "Your Form have been recieved, Thank you";
+?>
